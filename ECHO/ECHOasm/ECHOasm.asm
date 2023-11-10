e3fadce312db21727514a7965e840aca707fcc5d
flxgttr <agatazbirek@outlook.com>
2023-11-10 22:44:36 +0100

    args asm

--- ECHO/ECHOasm/ECHOasm.asm
+++ ECHO/ECHOasm/ECHOasm.asm
@@ -1,16 +1,18 @@
 ;rcx adres pierwszego elementu
 ;rdx liczba elementow
 ;r8 index
-;r9 adres kopii
+;r9 to adres kopii
 
 .data
-przesuniecie	word		24
-przesunieciex4	word		96
+przesuniecie	dword		24
+przesunieciex4	dword		96
 
-;WORD is an unsigned 16-bit value (0 to +65535 range)
-stride			word		0
-ostatniakolumna word		0	;width * 3
-len				word		0
+stride			dword		0
+
+ostatniakolumna dword		0	;width * 3
+len				dword		0
+
+
 
 .code
 GenerujEcho proc
@@ -18,22 +20,23 @@ cmp RDX, 0
 add RCX, R8 ;dodajemy index
 add R9, R8
 
-pop RAX
-pop RAX ;index?
-pop RAX
-pop RAX
-pop RAX
-mov R10, 3
-mul R10
+mov RAX, [rsp + 56] ;stride
+mov stride, EAX
 
-;mov ostatniakolumna, R10
+mov RAX, [rsp + 48] ;len
+mov len, EAX
+
+;cos w tym jest zle
+;mov RAX, [rsp + 40] ;width
+;mov R10, 3
+;mul R10 ;wynik jest w RAX
+;mov ostatniakolumna, EAX
+
 jle koniec
 
 dodawanie:
 movzx RBX, BYTE PTR[RCX]
-movzx R9, BYTE PTR[RCX]
 add RBX, 5
-;mov RBX, R9
 mov [RCX], BL ;rozmiar!!
 inc RCX
 dec RDX
