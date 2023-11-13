59a38d6934cfe718f3c70b34718da023473c1e82
flxgttr <agatazbirek@outlook.com>
2023-11-13 16:28:30 +0100

    dziala poza przeklejonym ofc

--- ECHO/ECHOasm/ECHOasm.asm
+++ ECHO/ECHOasm/ECHOasm.asm
@@ -4,12 +4,11 @@
 ;r9 to adres kopii
 
 .data
-przesuniecie		dword		24
 przesunieciex4		dword		96
 
 stride				dword		0
 
-ostatniakolumna		dword		0	;width * 3
+ostatniakolumna		qword		0	;width * 3
 len					dword		0
 
 kolumna				dword		0
@@ -30,10 +29,10 @@ mov RAX, [rsp + 48] ;len
 mov len, EAX
 
 mov RAX, [rsp + 40] ;width
-mov ostatniakolumna, EAX
-add EAX, ostatniakolumna
-add EAX, ostatniakolumna
-mov ostatniakolumna, EAX
+mov ostatniakolumna, RAX
+add RAX, ostatniakolumna
+add RAX, ostatniakolumna
+mov ostatniakolumna, RAX
 
 mov R11, RDX
 xor RDX, RDX
@@ -57,30 +56,23 @@ movzx RAX, BYTE PTR[R9]
 
 mov R11, RAX
 mov EAX, kolumna
-cmp EAX, ostatniakolumna ;if (kolumna != ostatnia_kolumna)
+cmp RAX, ostatniakolumna ;if (kolumna != ostatnia_kolumna)
 je kolumnatoostatniakolumna
-mov RAX, R11
 
-;musimy sprawdzic przepelnienie
-mov R11, 255
-
-add RBX, RAX
-cmp RBX, 255
-cmovg RBX, R11
-
-xor R11, R11
-cmp RBX, 0
-cmovl RBX, R11
-
-mov [RCX], BL ;rozmiar!!
+sub RCX, 24 
+sub RCX, wskaznikrgb
+xor RAX, RAX
+mov EAX, wiersz
+mov R12, RDX
+mul stride 
+mov RDX, R12 
+sub RCX, RAX
+cmp RCX, 0
+jg nielewagranica
 
 inc kolumna
 
-inc RCX
-inc R9
-
 xor R8, R8
-mov R11, RAX
 mov R12, RDX
 mov EAX, wiersz
 mul stride
@@ -103,9 +95,6 @@ jmp koniecpetli
 
 koniecpetli:
 
-inc RCX
-inc R9
-
 ;index_wzgledny = wiersz * stride + kolumna
 xor R8, R8
 mov R11, RAX
@@ -121,7 +110,73 @@ mov RDX, R12
 dec RDX ;dlugosc tablicy - 1
 cmp RDX, 0 ; dlugosc tablicy > 0?
 jg dodawanie ;while(wskaznik < dlugosc_tablicy + index + wartosci_rgb)
+jmp koniec
 
+nielewagranica:
+mov RCX, wskaznikrgb
+add RCX, R8
+add RCX, 24
+sub RCX, wskaznikrgb_cpy
+xor RAX, RAX
+mov EAX, wiersz
+mov R12, RDX
+mul stride 
+mov RDX, R12
+sub RCX, RAX
+sub RCX, ostatniakolumna
+cmp RCX, 0
+jl nieprawagranica
+inc kolumna
+jmp koniecpetli
+
+nieprawagranica:
+mov RAX, R11
+mov R9, wskaznikrgb_cpy
+sub R9, 24
+add R9, R8
+
+movzx RAX, BYTE PTR[R9]
+
+mov R11, RDX
+
+mov R12, 2
+mul R12
+mov R12, 10
+div R12
+
+mov RDX, R11
+
+;musimy sprawdzic przepelnienie
+mov RCX, wskaznikrgb
+add RCX, R8
+
+mov RBX, RAX
+
+mov R11, 255
+cmp RBX, 255
+cmovg RBX, R11
+
+xor R11, R11
+cmp RBX, 0
+cmovl RBX, R11
+
+mov [RCX], BL ;rozmiar!!
+
+inc kolumna
+
+;(wskaznik_cpy - przesunicie * 4) > (wartosci_rgb_cpy + wiersz * stride) przeklejone nie przetestowane
+sub RCX, 96 
+sub RCX, wskaznikrgb
+xor RAX, RAX
+mov EAX, wiersz
+mov R12, RDX
+mul stride 
+mov RDX, R12 
+sub RCX, RAX
+cmp RCX, 0
+jg nielewagranicaxcztery
+
+jmp koniecpetli
 
 GenerujEcho endp
 end
