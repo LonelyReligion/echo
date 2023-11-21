759c8daddf45e72b8400202cb3ea136c2bb6eb18
flxgttr <agatazbirek@outlook.com>
2023-11-21 14:06:10 +0100

    wrcamy do taktyki iterowania, dziala wielowatkowo
    bugs: artefakty co jakis czas, moze cos jeszcze trzeba schowac

--- ECHO/ECHOasm/ECHOasm.asm
+++ ECHO/ECHOasm/ECHOasm.asm
@@ -1,68 +1,263 @@
 ;rcx adres pierwszego elementu
-;rdx->r12 liczba elementow
+;rdx -> r12 liczba elementow
 ;r8 index
-;R9 to adres kopii
+;r9 to adres kopii
+
+.data
+stride				qword		0
+ostatniakolumna		qword		0	
+
+wskaznikrgb			qword		0
+wskaznikrgb_cpy		qword		0
 
 .code
 GenerujEcho proc
-dec R8
-mov R12, RDX 
+mov R15, R8
 
-inkrementacja_indexu:
-inc R8
-dec R12
+mov wskaznikrgb, RCX
+mov wskaznikrgb_cpy, R9
+
+mov RAX, [rsp + 48] ;stride
+mov stride, RAX
+
+mov RAX, [rsp + 40] 
+mov ostatniakolumna, RAX
+
+mov R12, RDX
+
+xor RDX, RDX
+xor RAX, RAX
+mov RAX, R8
+div stride ;F7 /6 	DIV r/m32 	M 	Valid 	Valid 	Unsigned divide EDX:EAX by r/m32, with result stored in EAX := Quotient, EDX := Remainder.
+mov R13, RAX
+mov R14, RDX
+
+cmp R12, 0 
+jle koniec
 
 dodawanie:
-cmp R12, 0
-jl koniec
+;wskaznik < dlugosc_tablicy + index + wartosci_rgb
+xor RAX, RAX
+mov RAX, R8
+sub RAX, R15
+sub RAX, R12
+cmp RAX, 0 
+jge koniec 
 
-; sprawdzamy czy jestesmy co najmniej 3 elementy od krawedzi
-mov R10, R8
-sub R10, 3
-cmp R10, 0
-jl inkrementacja_indexu
-;
+mov RAX, R14
+cmp RAX, ostatniakolumna ;if (R14 != ostatnia_kolumna)
+je kolumnatoostatniakolumna
 
-add RCX, R8
-
+mov R9, wskaznikrgb_cpy
 add R9, R8
-sub R9, 3
 
-movups xmm0, [R9]
-movups [RCX], xmm0
+;(wskaznik - przesunicie) > (wartosci_rgb + R13 * stride)
+mov RCX, R8
+sub RCX, 24
+xor RAX, RAX
+mov RAX, R13
+mul stride 
+sub RCX, RAX
+cmp RCX, 0
+jg nielewagranica
+inc R14
 
-sub RCX, R8
-sub R9, R8
-add R9, 3
+;index_wzgledny = R13 * stride + R14
+mov RAX, R13
+mul stride
+add RAX, R14
+mov R8, RAX 
 
-add R8, 16 ;index
-sub R12, 16;liczba elementow do przerobienia
-jz koniec
-cmp R12, 16
-jge dodawanie
-jmp dodawanie_liniowo
-
-dodawanie_liniowo:
-cmp R12, 0
-jl koniec
-
-add RCX, R8
-
-add R9, R8
-sub R9, 3
-
-mov CL, byte ptr[R9]
-
-sub RCX, R8
-sub R9, R8
-add R9, 3
-
-dec R12
-inc R8
-jmp dodawanie_liniowo
+jmp dodawanie
 
 koniec:
 ret
+
+kolumnatoostatniakolumna:
+inc R13
+mov R14, 0
+jmp koniecpetli
+
+koniecpetliikolumna:
+inc R14
+
+koniecpetli:
+;index_wzgledny = R13 * stride + R14
+mov RAX, R13
+mul stride
+add RAX, R14
+mov R8, RAX 
+
+jmp dodawanie
+
+nielewagranica:
+;(wskaznik + przesunicie) < (wartosci_rgb + R13 * stride + ostatnia_kolumna)
+mov RCX, R8
+add RCX, 24
+
+xor RAX, RAX
+mov RAX, R13
+
+mul stride 
+
+sub RCX, RAX
+sub RCX, ostatniakolumna
+cmp RCX, 0
+
+jl nieprawagranica
+jmp koniecpetliikolumna
+
+nieprawagranica:
+;*wskaznik = *(wskaznik_cpy - przesunicie) * 0.2;
+mov R9, wskaznikrgb_cpy
+sub R9, 24
+add R9, R8
+
+mov RDX, wskaznikrgb_cpy
+
+movzx RAX, BYTE PTR[R9]
+
+mov R11, 2
+mul R11
+mov R11, 10
+div R11
+
+mov RCX, wskaznikrgb
+add RCX, R8
+
+mov RBX, RAX
+
+;musimy sprawdzic przepelnienie
+mov R11, 255
+cmp RBX, 255
+cmovg RBX, R11
+
+xor R11, R11
+cmp RBX, 0
+cmovl RBX, R11
+
+mov [RCX], BL ;rozmiar!!
+
+;(wskaznik_cpy - przesunicie * 4) > (wartosci_rgb_cpy + R13 * stride) przeklejone
+mov RCX, R8
+sub RCX, 96 
+xor RAX, RAX
+mov RAX, R13
+mul stride 
+sub RCX, RAX
+cmp RCX, 0
+jg nielewagranicaxcztery
+jmp niexcztery
+
+niexcztery:
+;*wskaznik += *(wskaznik_cpy + przesunicie) * 0.8
+mov R9, wskaznikrgb_cpy
+add R9, R8
+add R9, 24
+
+movzx RAX, BYTE PTR[R9]
+
+xor RDX, RDX
+mov R11, 8
+mul R11
+mov R11, 10
+div R11
+
+
+mov RCX, wskaznikrgb
+add RCX, R8
+movzx RBX, BYTE PTR[RCX]
+
+add RBX, RAX
+ 
+;musimy sprawdzic przepelnienie
+mov R11, 255
+cmp RBX, 255
+cmovg RBX, R11
+
+xor R11, R11
+cmp RBX, 0
+cmovl RBX, R11
+
+mov [RCX], BL ;rozmiar!!
+
+jmp koniecpetliikolumna
+
+nielewagranicaxcztery:
+; (wskaznik_cpy + przesunicie * 4) < (wartosci_rgb_cpy + R13 * stride + ostatnia_kolumna)
+mov RCX, wskaznikrgb_cpy
+add RCX, R8
+add RCX, 96
+sub RCX, wskaznikrgb_cpy
+xor RAX, RAX
+mov RAX, R13
+mul stride 
+sub RCX, RAX
+sub RCX, ostatniakolumna
+cmp RCX, 0
+jl nieprawagranicaxcztery
+jmp niexcztery
+
+nieprawagranicaxcztery:
+;*wskaznik += *(wskaznik_cpy - przesunicie * 4) * 0.1;
+mov RCX, wskaznikrgb
+add RCX, R8
+
+mov R9, wskaznikrgb_cpy
+add R9, R8
+sub R9, 96
+
+movzx RBX, BYTE PTR[RCX]
+movzx RAX, BYTE PTR[R9]
+
+xor RDX, RDX
+mov R11, 10
+div R11
+
+add RBX, RAX
+
+;musimy sprawdzic przepelnienie
+mov R11, 255
+cmp RBX, 255
+cmovg RBX, R11
+
+xor R11, R11
+cmp RBX, 0
+cmovl RBX, R11
+
+mov [RCX], BL ;rozmiar!!
+
+;*wskaznik += *(wskaznik_cpy + przesunicie) * 0.7;
+mov R9, wskaznikrgb_cpy
+add R9, R8
+add R9, 24
+
+movzx RAX, BYTE PTR[R9] ;<-wyjatkogenne
+
+xor RDX, RDX
+mov R11, 10
+div R11
+mov R11, 7
+mul R11
+
+mov RCX, wskaznikrgb
+add RCX, R8
+movzx RBX, BYTE PTR[RCX]
+
+add RBX, RAX
+
+;musimy sprawdzic przepelnienie
+mov R11, 255
+cmp RBX, 255
+cmovg RBX, R11
+
+xor R11, R11
+cmp RBX, 0
+cmovl RBX, R11
+
+mov [RCX], BL ;rozmiar!!
+
+jmp koniecpetliikolumna
 
 GenerujEcho endp
 end
